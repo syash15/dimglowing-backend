827a8b614efd6789c9110b6dc8fc9952ede27f5d
Yash Sanjay Patil <syashpatil15@example.com>
2026-03-02 00:18:05 +0530

    added forgot password system

--- server.js
+++ server.js
@@ -341,6 +341,22 @@ app.get("/products", async (req, res) => {
   }
 });
 
+/* ================= GET PROFILE ================= */
+
+app.get("/profile", verifyToken, async (req, res) => {
+  try {
+
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user)
+      return res.status(404).json({ error: "User not found" });
+
+    res.json(user);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 /* ================= START SERVER ================= */
 
